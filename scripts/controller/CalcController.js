49eaf95be9669894a14dc22039d07d80d6a1950e
eduardopireslucio1 <eduardopireslucioo@gmail.com>
2022-01-18 14:55:34 -0300

    ctrl c + ctrl v events

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -17,6 +17,33 @@ class CalcController {
 
     }
 
+    copyToClipboard() {
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+
+    }
+
+    pastFromClipboard() {
+
+        document.addEventListener('paste', e => {
+
+            let text = e.clipboardData.getData("Text");
+            this.displayCalc = parseFloat(text);
+
+        });
+
+    }
+
     initialize() {
 
         this.setDisplayDateTime()
@@ -28,6 +55,7 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pastFromClipboard();
 
     }
 
@@ -77,7 +105,9 @@ class CalcController {
                 case '9':
                     this.addOperation(parseInt(e.key));
                     break;
-
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
+                    break
             }
 
         })
