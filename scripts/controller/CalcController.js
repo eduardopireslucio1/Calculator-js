453b4794e03b2c1d4dfded1108f21becc5c8431e
eduardopireslucio1 <eduardopireslucioo@gmail.com>
2022-01-19 09:38:37 -0300

    add audio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,8 +2,10 @@ class CalcController {
 
     constructor() {
 
-        this.lastOperator = '';
-        this.lastNumber = '';
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
+        this._lastOperator = '';
+        this._lastNumber = '';
 
         this._operation = [];
         this._locale = 'pt-BR';
@@ -57,11 +59,40 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pastFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+
+            btn.addEventListener('dblclick', e => {
+
+                this.toggleAudio();
+
+            });
+
+        });
+
+    }
+
+    toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio() {
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
     }
 
     initKeyboard() {
 
         document.addEventListener('keyup', e => {
+
+            this.playAudio();
 
             console.log(e.key);
 
@@ -326,6 +357,8 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio();
 
         switch (value) {
 
