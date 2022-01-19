e85d6926b17b2607aba1d6a76b85bd260aa19583
eduardopireslucio1 <eduardopireslucioo@gmail.com>
2022-01-19 11:24:19 -0300

    ultimo commit

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -204,8 +204,15 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""));
+        try {
+            return eval(this._operation.join(""));
+        } catch (e) {
+            setTimeout(() => {
 
+                this.setError();
+
+            }, 1);
+        }
     }
 
     calc() {
@@ -485,6 +492,13 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+
+            this.setError();
+            return false;
+
+        }
 
         this._displayCalcEl.innerHTML = value;
 
